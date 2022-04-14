313cce580047f7aa6069896b3fdbcc8a51920ef4
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2022-04-14 05:18:41 -0700

    handel bars

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,12 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Abbas Mahdavi" });
 });
 
+router.get('/postimage', (req,res,next) => {
+  res.render('postimage');
+});
+
+router.get('/viewpost', (req,res,next) => {
+  res.render('viewpost');
+});
+
 router.get('/login', (req,res,next) => {
   res.render('login');
-})
+});
 
 router.get('/register', (req,res,next) => {
   res.render('register');
-})
+});
 
 module.exports = router;
