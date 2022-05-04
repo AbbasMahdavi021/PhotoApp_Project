0e68de1d1a64c1bf48517340ed951df66b0f750f
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2022-05-04 01:37:32 -0700

    bcrypt

--- application/routes/users.js
+++ application/routes/users.js
@@ -3,6 +3,19 @@ var router = express.Router();
 var db = require('../config/database');
 const { errorPrint, successPrint } = require('../helpers/debug/debugprinters');
 const UserError = require('../helpers/error/UserError');
+var bcrypt = require ('bcrypt');
+
+
+// db.query("insert into users (username, email, password, active, created) value (?,?,?,?,now(),", 
+// ["test123", "test123.email.com", "123pass", 1], 
+// function(err,results, fields) {
+//   if(err) {
+//     console.error(err);
+//   }else {
+//     console.log(results);
+//   }
+//   db.end;
+// })
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -18,10 +31,18 @@ router.post('/login', (req, res, next) => {
    * do server side validation
    */
 
-  let baseSQL = "SELECT username, password FROM users WHERE username=? AND password=?;"
+  let baseSQL = "SELECT username, password FROM users WHERE username=?;"
   db.execute(baseSQL,[username, password])
   .then(([resutls,fields]) => {
     if(resutls && resutls.lenght == 1){
+      let hasedPassword = resutls[0].password;
+      return bcrypt.compare(password, hasedPassword);
+    }else{
+      throw new UserError("invalid username and/or password!", "/login", 200);
+    }
+  })
+  .then((passwordsMatched) => {
+    if(passwordsMatched){
       successPrint(`User ${username} is logged in.`);
       res.locals.logged = true;
       res.render('index');
@@ -32,7 +53,7 @@ router.post('/login', (req, res, next) => {
   .catch((err) =>{
     errorPrint("user login failed");
     if(err instanceof UserError){
-      errorPrint(err.getMessage());
+    errorPrint(err.getMessage());
       res.status(err.getStatus());
       res.redirect('/login');
     }else{
@@ -68,8 +89,7 @@ router.post('/register' , (req, res, next) => {
     })
     .then(([results, fields]) => {
       if (results && results.lenght == 0) {
-        let baseSQL = "insert into users (username, email, password, created) values ?,?,?,now());"
-        return db.execute(baseSQL,[username, email, password])
+        return bcrypt.hash(password,15);
       } else {
         throw new UserError(
           "Registeration Failed: Email already exists",
@@ -77,6 +97,10 @@ router.post('/register' , (req, res, next) => {
           200
         );
       }
+    })
+    .then((hasedPassword) => {
+      let baseSQL = "insert into users (username, email, password, created) values ?,?,?,now());"
+      return db.execute(baseSQL,[username, email, hasedPassword])
     })  
     .then((results, fields) => {
       if(results && results.affectedRows){
